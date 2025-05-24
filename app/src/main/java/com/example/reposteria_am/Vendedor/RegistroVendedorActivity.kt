5e5d54d41135699db1df86214ad2a488e0ac745b
RonalAlexanderEs <ronalalexanderescuderosilva@gmail.com>
2025-05-23 20:23:24 -0500

    Registro de vendedor

---
+++ app/src/main/java/com/example/reposteria_am/Vendedor/RegistroVendedorActivity.kt
@@ -0,0 +1,14 @@
+package com.example.reposteria_am.Vendedor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
