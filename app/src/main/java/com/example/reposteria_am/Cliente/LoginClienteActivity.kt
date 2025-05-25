cba588a22fd66b895f229d67008db0e0531eb064
RonalAlexanderEs <ronalalexanderescuderosilva@gmail.com>
2025-05-25 01:36:52 -0500

    Arreglo de bugs y errores implemntacion login usuario

--- app/src/main/java/com/example/reposteria_am/Cliente/LoginClienteActivity.kt
+++ app/src/main/java/com/example/reposteria_am/Cliente/LoginClienteActivity.kt
@@ -1,13 +1,16 @@
 package com.example.reposteria_am.Cliente
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
+
 import com.example.reposteria_am.R
 
-class RegistroClienteActivity : AppCompatActivity() {
+class LoginClienteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro_cliente)
+
+        setContentView(R.layout.activity_login_cliente)
+
     }
 }
